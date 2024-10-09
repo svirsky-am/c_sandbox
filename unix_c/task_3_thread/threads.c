b11a8ec6210521db7ee66890b5d7ad206e412663
svirsky <aouta91alk@gmail.com>
2024-10-09 23:06:26 +0300

    add strucr meta_of_array for thred task 3

--- unix_c/task_3_thread/threads.c
+++ unix_c/task_3_thread/threads.c
@@ -42,35 +42,105 @@ void print_vect(int * v, int n)
    printf("%i\n", v[n-1]);
 }
 
+int numberCount(FILE* input) {
+  fseek(input, 0, SEEK_SET);
+  int counter = 0;
+  while (1) {
+    int value;
+    if (fscanf(input, "%d", &value) == 1)
+      counter++;
+    if (feof(input))
+      break;
+  }
+  return counter;
+}
+
+void read_numbers(FILE* input, int size, int* numbers) {
+  fseek(input, 0, SEEK_SET);
+  for (int i = 0; i < size; ++i) {
+    fscanf(input, "%d", &numbers[i]);
+  }
+}
+
+void print_array(int* numbers, int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d ; ", numbers[i]);
+  }
+  printf("\n");
+}
+
+struct meta_of_array{
+    int size;
+    int worker_count;
+    int* numbers;
+    int acc_sum;
+};
+
 void * thread_func(void *arg)
 {
-   int i;
-   int * v;
+  int size = * (int *) arg;
+  printf("Size %d\n", size);
+  int i;
+  int worker_id = * ((int * ) arg + 1);
+  // worker_id ++;
+  
    
-  //  Приведение типа (умножение указателя на int)
-   int size = * (int *) arg; 
-      v = malloc(sizeof(int)*size);
-   for(i = 0; i < size; i++) v[i] = i+1;
-   print_vect(v, size);
-   while(next_permutation(v, size)) {
-     print_vect(v, size);
-     sync();
-   }
-   free(v);
+  // //  Приведение типа (умножение указателя на int)
+  //  int size = * (int *) arg; 
+  //     v = malloc(sizeof(int)*size);
+  //  for(i = 0; i < size; i++) v[i] = i+1;
+   printf("Size %d, worker_id %d\n", size, worker_id);
+  //  print_vect(v, size);
+
+  // //  print_array(arg, size);
+
+  //  printf("Size %d\n", size);
+  // //  while(next_permutation(v, size)) {
+  // //    print_vect(v, size);
+  // //    sync();
+  // //  }
+  //  free(v);
 }
+
+
 
 int main(int argc, char * argv[])
 {
-   int size1, size2, result;
+  FILE* input = fopen("INPUT.TXT", "r");
+  if (input == 0) {
+    printf("cant open file\n");
+    return 1;
+  }
+  int size = numberCount(input);
+  int* numbers = (int*)malloc(sizeof(int) * size);
+  read_numbers(input, size, numbers);
+  print_array(numbers, size);
+
+
+
+   int  result;
    pthread_t thread1, thread2;
-   size1 = 4;
-   result = pthread_create(&thread1, NULL, thread_func, &size1);
+
+   struct meta_of_array meta_arr;
+   meta_arr.acc_sum = 0;
+   meta_arr.numbers = numbers;
+   meta_arr.size = size;
+   meta_arr.worker_count = 0;
+
+
+
+  //  size = 4;
+   result = pthread_create(&thread1, NULL, thread_func,  &meta_arr);
    if (result != 0) {
      perror("Creating the first thread");
      return EXIT_FAILURE;
    }
-   size2 = 5;
-   result = pthread_create(&thread2, NULL, thread_func, &size2);
+  //  size2 = 5;
+
+
+
+   
+   result = pthread_create(&thread2, NULL, thread_func, &meta_arr );
    if (result != 0) {
      perror("Creating the second thread");
      return EXIT_FAILURE;
