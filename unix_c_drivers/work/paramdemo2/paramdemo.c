5d921dfb32a484e6fcb7f6f9f7aa8f065ae92eb7
a <aouta91alk@gmial.com>
2024-12-23 15:26:52 +0300

    tuneup param 2

--- unix_c_drivers/work/paramdemo2/paramdemo.c
+++ unix_c_drivers/work/paramdemo2/paramdemo.c
@@ -9,7 +9,30 @@ MODULE_AUTHOR("svirsky");
 
 static int value1 = 1;
 static int value2 = 1;
-// static char *m_char = "empty";
+/*---------------------- value set func--------------------------------*/
+
+static int value_set(const char *val, const struct kernel_param *kp )
+{   
+    int old = value2;
+    int ret, res;
+
+    ret = kstrtoint(val, 10, &res);
+    if(ret != 0 || res < 0 || res > 100) return -EINVAL;
+
+    param_set_int(val, kp);
+
+    printk(KERN_ALERT MOD_NAME " old value = %d, new value = %d\n", old, value2);
+    return 0;
+};
+
+
+/*---------------------- callback structure--------------------------------*/
+
+static const struct kernel_param_ops kpops = {
+    .set = value_set, 
+    .get = param_get_int
+};
+
 
 /*----------------- declare module patams ------------------- */
 /* two macroses  module_param() and MODULE_PARM_DESC() */
@@ -17,7 +40,7 @@ static int value2 = 1;
 module_param(value1, int, 0664);
 MODULE_PARM_DESC(value1, "module counter");
 
-module_param(value2, int, 0664);
+module_param_cb(value2, &kpops, &value2, 0664);
 MODULE_PARM_DESC(value2, "module string");
 
 int init_module()
